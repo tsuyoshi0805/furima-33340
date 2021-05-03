ba07255ebf1a55bebc1a91f0c21f8b4dd146acb5
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-05-03 15:02:32 +0900

    testコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+it "nicknameが空だと登録できない" do
+end
+it "emailが空では登録できない" do
+end
+it "passwordが空では登録できない" do
+end
+it "family_nameが空では登録できない" do
+end
+it "first_nameが空では登録できない" do
+end
+it "family_name_kanaが空では登録できない" do
+end
+it "first_name_kanaが空では登録できない" do
+end
+it "birthdayが空では登録できない" do
+end
+end
+end
