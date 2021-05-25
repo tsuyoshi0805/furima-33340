0c9864d6d4802e5fda5a6ba11c59b65e3cb03372
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-05-25 19:16:02 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-     
+    
      @items = Item.includes(:user).order("created_at ASC")
   end
 
