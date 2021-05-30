9d0f772f3cba72f4022c1e29602379569bc6801e
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-05-30 16:23:38 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
  
   def edit
     @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to root_path
+    end
 
   end
   def update
