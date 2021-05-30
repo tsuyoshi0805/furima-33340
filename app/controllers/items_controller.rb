9f2088ee4d42cc83f18863fbe5903c52dee3b48c
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-05-30 17:59:39 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :unless_item, only: [:edit, :update]
+
+
   def index
     
      @items = Item.includes(:user).order("created_at DESC")
@@ -19,18 +23,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-     @item = Item.find(params[:id])
+     
   end
  
   def edit
-    @item = Item.find(params[:id])
-    unless @item.user.id == current_user.id
-      redirect_to root_path
-    end
 
   end
   def update
-       @item = Item.find(params[:id])
+      
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,6 +42,15 @@ end
 
   def item_params
     params.require(:item).permit(:name, :price, :description, :category_id, :status_id, :shipping_cost_id, :shipping_day_id,
-                                 :category_id, :image, :shipping_area_id).merge(user_id: current_user.id)
+                                 :category_id, :image, :shipping_area_id).merge(user_id: current_user.id)              
   end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+def unless_item
+  unless @item.user.id == current_user.id
+    redirect_to root_path
+end
+end
+
 end
