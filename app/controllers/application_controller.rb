927fed4c50251112dbcd78ecc7bdba6551d938a7
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-07-04 06:32:36 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
   private
 
   def configure_permitted_parameters
@@ -8,4 +8,9 @@ class ApplicationController < ActionController::Base
                                       keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
                                              :birthday])
   end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+end
 end
